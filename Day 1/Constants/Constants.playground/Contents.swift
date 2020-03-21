deff0344ced083444fa6fc4bb0b3d8689da424ae
David Leonard <david@minglebit.com>
2020-03-21 19:47:49 +0200

    Day 1 - Constants

---
+++ Day 1/Constants/Constants.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// To create a constant you need to use the let keyword
+let taylor = "swift"
+
+// let taylor = "not so swift"
+// ERROR:
+// Invalid redeclaration of 'taylor'
+
+// Declare more constant on a single line
+let you = "you", i = "I", they = "they"
+
