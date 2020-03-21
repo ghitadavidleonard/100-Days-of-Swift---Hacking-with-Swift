1b2d84be25aafaaab27cc25198cfc709afac5aed
David Leonard <david@minglebit.com>
2020-03-21 19:33:17 +0200

    Day 1 - String interpolation

---
+++ Day 1/String interpolation/StringInterpolation.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// You can place ant type of variable inside your string using \
+// followed by your variable name in paranthesses
+
+var score = 85
+var str = "Your score was \(score)"
+
+// You can make strings out of strings
+
+var result = "The test results are here: \(str)"
+
+var firstNumber = 3
+var secondNumber = 4
+
+var addition = "\(firstNumber) + \(secondNumber) = \(3 + 4)"
