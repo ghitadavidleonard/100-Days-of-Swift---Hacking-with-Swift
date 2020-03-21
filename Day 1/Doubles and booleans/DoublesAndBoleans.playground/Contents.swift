38a48ef29b6f84e83fe2410c64f50ab40d406a4a
David Leonard <david@minglebit.com>
2020-03-21 19:24:24 +0200

    Day 1 - Doubles and booleans

---
+++ Day 1/Doubles and booleans/DoublesAndBoleans.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// Variable with type of double
+var pi = 3.141
+// If you do not specify a type for a floating-point literal, Swift infers that you want
+//to create a Double
+
+var floatingNumber: Float = 3.14151617
+
+// Variable with type of boolean
+var awesome = true
+
+// Double precision is of at least 15 decimal digits
+var testPrecision = 3.141516171819202
+
+//If you combine integer and floating-point in an expression, a type of Double will be inferred
+let anotherPi = 3 + 0.14159
+
+// Boolean values are useful when working with conditional
+let bananasAreDelicious = true
+if bananasAreDelicious {
+    print("Mmm, tasty bananas!")
+}else{
+    print("Eww, bananas are horrible.");
+}
