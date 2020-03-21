04d879e638c2d08d4220bba93b0dc7fa4871fb36
David Leonard <david@minglebit.com>
2020-03-21 20:34:26 +0200

    Day 1- Simple types: Summary

---
+++ Day 1/Simple types- Summary/SimoleTypesSummary.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// First
+var str = "Hello, playground"
+var taylor = "swift"
+
+// Second
+var name = "David Leonard"
+var details = """
+David Leonard
+Ghita
+Age 21
+"""
+
+// Third
+var age = 21
+var pi = 3.1415
+var anAppleADayKeepsTheDoctorAway = true
+
+// Fourth
+let stringMadeOutOfStrings = "\(str), my name is \(name) and I have \(age)"
+
+// Fifth
+let rock : String = "stone"
+let density: Double = 2.28
