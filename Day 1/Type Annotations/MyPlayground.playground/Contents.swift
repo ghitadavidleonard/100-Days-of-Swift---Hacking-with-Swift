b9b0a35f8465d80e4e125cbd808d47507a633e0e
David Leonard <david@minglebit.com>
2020-03-21 20:20:18 +0200

    Day 1 - Type Annotation

---
+++ Day 1/Type Annotations/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+// This will make str a string. So you can't try to assign it an integer or a boolean
+
+// str = true
+// ERROR:
+// Cannot assign value of type 'Bool' to type 'String'
+
+// If you want, you can be explicit about the type of your data
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+let weight = 81.512345
+
+// Notice that boolean and integers have the short type: Bool, Int
+let noticeMe: Bool = true;
+let noticeMeToo: Int = 2;
