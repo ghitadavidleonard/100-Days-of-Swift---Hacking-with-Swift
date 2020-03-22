8cc50fbad56fccf5cdd63539711dc4a4f18255b6
David Leonard <david@minglebit.com>
2020-03-22 20:44:20 +0200

    Day 2 - Arrays vs Sets vs Tuples

---
+++ Day 2/ArraysVsSetsVsTuples.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// If you need a specific, fixed collection of related values, you should use a tuple
+let address = (house: 555, street: "Taylor Swift")
+
+// If you need a collection of values that must be unique or you need to be able to check whether
+// a specific item is in there extremly quickly, you should use a set
+let set = Set(["aardvark", "astronaut", "azalea"])
+
+// If you need a collection of values that can contain duplicates, or the order of your items
+// matters, use an array
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+
+// Arrays are by far the most common of the three types
+
+
