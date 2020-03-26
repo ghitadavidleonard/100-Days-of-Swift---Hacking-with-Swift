a79114facc8573f17f5a74e79ca6ac531ae45b1a
David Leonard <david@minglebit.com>
2020-03-26 20:07:44 +0200

    Day 3 - Operators

---
+++ Day 3/ArithmeticOperators.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// Operators
+let firstScore = 12
+let secondScore = 4
+
+// Add and substract
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+// Multiply and divide
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+// Remainders after division
+let reminder = 13 % 4
+ 
+let play = ((firstScore  * firstScore) / (secondScore + firstScore - secondScore))
+    + (secondScore * firstScore)
