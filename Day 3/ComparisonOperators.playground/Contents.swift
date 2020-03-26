639e6a0f061a3495eafbf739a7c81a8a86704264
David Leonard <david@minglebit.com>
2020-03-26 21:06:26 +0200

    Day 3 - Comparison Operators

---
+++ Day 3/ComparisonOperators.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let firstScore = 6
+let secondScore = 4
+
+// Operators that check for equality: '==' -> checks two values are the same
+//                                    '!=' -> checks two values are not the same
+
+firstScore == secondScore
+firstScore != secondScore
+
+// There are four operators for comparing whether one value is greater than, less that, or
+// equal to another. These are just like in mathematics
+
+firstScore < secondScore
+firstScore >= secondScore
+secondScore <= firstScore
+secondScore > firstScore
+
+// Each of thse alson work with strings, because strings have a natural alphabetical order
+"Taylor" <= "Swift"
