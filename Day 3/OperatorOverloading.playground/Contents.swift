4ad7889ddd7be99e55c3d7ae8e09c6efe3110a7b
David Leonard <david@minglebit.com>
2020-03-26 20:33:41 +0200

    Day 3 - Operators overloading

---
+++ Day 3/OperatorOverloading.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Swift supports operator overoloading
+// For example + sums itegers like this
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+// But + also joins strings, like this
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+// You can even use + to join arrays, like this
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+
+let batles = firstHalf + secondHalf
+
