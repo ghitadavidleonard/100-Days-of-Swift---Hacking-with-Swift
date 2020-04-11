c542e26b289f016f00c41825b23c8a7e64990685
David Leonard <david@minglebit.com>
2020-04-11 23:05:31 +0300

    Day  4 - Infinite Loops

---
+++ Day 4/InfiniteLoops.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var counter = 0
+
+// to make an infinite loop just use true as your condition. True is always true
+while true {
+    print(" ")
+    counter += 1
+    
+    if counter == 273 {
+        break
+    }
+}
