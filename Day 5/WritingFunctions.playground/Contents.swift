8759cd54fe99c965b218818a95c19faf663a79dd
David Leonard <david@minglebit.com>
2020-04-13 20:50:47 +0300

    Day 5 - Writing function

---
+++ Day 5/WritingFunctions.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func printHelp() {
+    let message = """
+        Welcome to MyApp!
+
+        Run this app inside a directory of images and
+        MyApp will resize them all into thumbnails
+        """
+    
+    print(message)
+}
+
+// running a function is often reffered to as calling a function
+printHelp()
